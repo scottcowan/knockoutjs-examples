b06fb7231073b499f03195572d0525d3ddde49fa
Scott Cowan <info@scottcowan.org>
2012-04-15 16:24:52 +0100

    added hover to the plot

--- js/example-routes.js
+++ js/example-routes.js
@@ -14,6 +14,9 @@ $(function () {
 		},
 		"/flot/series": function () {
 			viewModel.currentView(new FlotSeriesModel());
+		},
+		"/flot/hover": function () {
+			viewModel.currentView(new FlotHoverModel());
 		},		
 		"/slickgrid/basic": function () {
 			viewModel.currentView(new SlickGridModel());
@@ -112,10 +115,52 @@ function FlotSeriesModel(){
 	var days = [1,	2,	3,	4,	7,	14,	21,	30,	60,	90,	120,	150,	180,	210,	240,	270,	300,	330,	360];
 	var bands = ["0 - 50k","50k - 250k","250k - 1m","1m - 10m","10m - 50m","50m - 100m","over 100m"];
 	
+	function daysToText(days){
+		console.log(days);
+		if(days==1)
+			return "1d";
+		if(days==2)
+			return "2d";
+		if(days==3)
+			return "3d";
+		if(days==4)
+			return "4d";
+		if(days==7)
+			return "1w";
+		if(days==14)
+			return "2w";
+		if(days==21)
+			return "3w";
+		if(days==30)
+			return "1m";
+		if(days==60)
+			return "2m";
+		if(days==90)
+			return "3m";
+		if(days==120)
+			return "4m";
+		if(days==150)
+			return "5m";
+		if(days==180)
+			return "6m";
+		if(days==210)
+			return "7m";
+		if(days==240)
+			return "8m";
+		if(days==270)
+			return "9m";
+		if(days==300)
+			return "10m";
+		if(days==330)
+			return "11m";
+		if(days==360)
+			return "1y";
+	}
+	
 	for(var i=0;i<7;i++){
 		var row = [];
 		for(var j = 0; j<19;j++){
-			row[j] = [days[j],150 + i*Math.log(j*100)];
+			row[j] = [days[j],Math.round(150 + i*Math.log(j*100))];
 		}
 		this.data().push(row);
 	}
@@ -136,14 +181,138 @@ function FlotSeriesModel(){
 		    for(var i=0;i<7;i++){
 		    	gridData[i] = {
 					data: valueUnwrapped[i],
-					lines: { show: true, fill: false },
 					label: bands[i]
 				};
 		    }
-			$.plot($(element),gridData);
+			$.plot($(element),gridData,{
+           			series: {
+               			lines: { show: true },
+               			points: { show: true }
+           			},
+           			grid: { hoverable: true}
+           		}
+			);
+			
+		    function showTooltip(x, y, contents) {
+		        $('<div id="tooltip">' + contents + '</div>').css( {
+		            position: 'absolute',
+		            display: 'none',
+		            top: y + 5,
+		            left: x + 5,
+		            border: '1px solid #fdd',
+		            padding: '2px',
+		            'background-color': '#fee',
+		            opacity: 0.80
+		        }).appendTo("body").fadeIn(200);
+		    }
+		
+		    var previousPoint = null;
+		    $(element).bind("plothover", function (event, pos, item) {
+		        $("#x").text(pos.x.toFixed(2));
+		        $("#y").text(pos.y.toFixed(2));
+		
+	            if (item) {
+	                if (previousPoint != item.dataIndex) {
+	                    previousPoint = item.dataIndex;
+	                    
+	                    $("#tooltip").remove();
+	                    var x = item.datapoint[0].toFixed(2),
+	                        y = item.datapoint[1].toFixed(2);
+	                    
+	                    showTooltip(item.pageX, item.pageY,
+	                    	item.series.label + " for " + daysToText(Math.floor(x)) + " = " + Math.floor(y) + " BPS");
+	                }
+	            }
+	            else {
+	                $("#tooltip").remove();
+	                previousPoint = null;            
+	            }
+		    });
 		}
 	};
 };
+
+function FlotHoverModel(){
+	this.template = "Flot/Hover"
+	this.data = ko.observableArray();
+		
+   ko.bindingHandlers.flot = {
+		init: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+		    // This will be called when the binding is first applied to an element
+		    // Set up any initial state, event handlers, etc. here
+		},
+		update: function(element, valueAccessor, allBindingsAccessor, viewModel) {
+		    // This will be called once when the binding is first applied to an element,
+		    // and again whenever the associated observable changes value.
+		    // Update the DOM element based on the supplied values here.
+		    //var options = allBindingsAccessor().datepickerOptions || {};
+		    var value = valueAccessor();
+		    var valueUnwrapped = ko.utils.unwrapObservable(value);
+		    var sin = [], cos = [];
+		    for (var i = 0; i < 14; i += 0.5) {
+		        sin.push([i, Math.sin(i)]);
+		        cos.push([i, Math.cos(i)]);
+		    }
+    
+			$.plot($(element),
+				[{ data: sin, label: "sin(x)"}, { data: cos, label: "cos(x)" } ], 
+				{
+           			series: {
+               			lines: { show: true },
+               			points: { show: true }
+           			},
+           			grid: { hoverable: true, clickable: true },
+           			yaxis: { min: -1.2, max: 1.2 }
+         		}
+         	);
+
+		    function showTooltip(x, y, contents) {
+		        $('<div id="tooltip">' + contents + '</div>').css( {
+		            position: 'absolute',
+		            display: 'none',
+		            top: y + 5,
+		            left: x + 5,
+		            border: '1px solid #fdd',
+		            padding: '2px',
+		            'background-color': '#fee',
+		            opacity: 0.80
+		        }).appendTo("body").fadeIn(200);
+		    }
+		
+		    var previousPoint = null;
+		    $("#placeholder").bind("plothover", function (event, pos, item) {
+		        $("#x").text(pos.x.toFixed(2));
+		        $("#y").text(pos.y.toFixed(2));
+		
+		        if ($("#enableTooltip:checked").length > 0) {
+		            if (item) {
+		                if (previousPoint != item.dataIndex) {
+		                    previousPoint = item.dataIndex;
+		                    
+		                    $("#tooltip").remove();
+		                    var x = item.datapoint[0].toFixed(2),
+		                        y = item.datapoint[1].toFixed(2);
+		                    
+		                    showTooltip(item.pageX, item.pageY,
+		                                item.series.label + " of " + x + " = " + y);
+		                }
+		            }
+		            else {
+		                $("#tooltip").remove();
+		                previousPoint = null;            
+		            }
+		        }
+		    });
+		
+		    $("#placeholder").bind("plotclick", function (event, pos, item) {
+		        if (item) {
+		            $("#clickdata").text("You clicked point " + item.dataIndex + " in " + item.series.label + ".");
+		            plot.highlight(item.series, item.datapoint);
+		        }
+		    });       	
+		}
+	};	
+}
 
 function SlickGridModel(){
 	this.template = "SlickGrid/Basic"
