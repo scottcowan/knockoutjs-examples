1277c04d9d69fba97021468aab819919efd91a29
Scott Cowan <info@scottcowan.org>
2012-05-03 11:45:57 +0100

    Added a datepicker with just a calendar

--- js/example-routes.js
+++ js/example-routes.js
@@ -24,8 +24,11 @@ $(function () {
 		"/slickgrid/editor": function () {
 			viewModel.currentView(new SlickGridEditorModel());
 		},
-		"/datepicker":function(){
+		"/datepicker/textbox":function(){
 			viewModel.currentView(new DatePickerModel());
+		},
+		"/datepicker/calendar":function(){
+			viewModel.currentView(new DatePickerCalModel());
 		}
 	});
                 
@@ -439,6 +442,41 @@ ko.bindingHandlers.slickGrid = {
 
 function DatePickerModel(){
 	this.template = "DatePicker";
+	
+	self.Tomorrow = nextDay(new Date());
+
+	self.StartDate = ko.observable(self.Tomorrow);
+	
+	ko.bindingHandlers.datepicker = {
+		init: function(element, valueAccessor, allBindingsAccessor) {
+			//initialize datepicker with some optional options
+			var options = allBindingsAccessor().datepickerOptions || {};
+			$(element).datepicker(options);
+
+			//handle the field changing
+			ko.utils.registerEventHandler(element, "change", function() {
+				var observable = valueAccessor();
+				observable($(element).datepicker("getDate"));
+			});
+
+			//handle disposal (if KO removes by the template binding)
+			ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+				$(element).datepicker("destroy");
+			});
+
+		},
+		update: function(element, valueAccessor) {
+			var value = ko.utils.unwrapObservable(valueAccessor());
+			//debugger;
+			if (isNaN(Date.parse(value))==false){
+				$(element).datepicker("setDate", value);
+			}
+		}
+	};
+}
+
+function DatePickerCalModel(){
+	this.template = "DatePickerCal";
 	
 	self.Tomorrow = nextDay(new Date());
 
